acd7936eee306544262b0a9e6234c2cee7346f64
Alin97 <alin_andrei97@yahoo.com>
2020-02-25 15:24:31 +0200

    Configured MyfitnessappApplication, application.properties and created BaseModel abstract class.

--- src/main/java/com/university/myfitnessapp/MyfitnessappApplication.java
+++ src/main/java/com/university/myfitnessapp/MyfitnessappApplication.java
@@ -2,8 +2,14 @@ package com.university.myfitnessapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EntityScan(basePackages = "com.university.myfitnessapp.repository.models")
+@EnableJpaRepositories(basePackages = "com.university.myfitnessapp.repository")
 public class MyfitnessappApplication {
 
     public static void main(String[] args) {
