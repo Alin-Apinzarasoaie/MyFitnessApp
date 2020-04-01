9badccb1187ace237a080dd78a8eb1c44cc131d2
Alin97 <alin_andrei97@yahoo.com>
2020-04-01 16:38:02 +0300

    Configured the Registration, Login and Logout form, also added Users and Roles.

--- src/main/java/com/university/myfitnessapp/MyfitnessappApplication.java
+++ src/main/java/com/university/myfitnessapp/MyfitnessappApplication.java
@@ -3,6 +3,7 @@ package com.university.myfitnessapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EntityScan(basePackages = "com.university.myfitnessapp.repository.models")
 @EnableJpaRepositories(basePackages = "com.university.myfitnessapp.repository")
+@EnableJpaAuditing
 public class MyfitnessappApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/university/myfitnessapp/repository/models/BaseModel.java
+++ src/main/java/com/university/myfitnessapp/repository/models/BaseModel.java
@@ -1,6 +1,8 @@
 package com.university.myfitnessapp.repository.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -8,14 +10,22 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.UUID;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @JsonIgnoreProperties(
-        value = {"createdAt", "updateAt"},
+        value = {"createdAt", "updateAt","id", "deleted"},
         allowGetters = true
 )
 public abstract class BaseModel implements Serializable {
+
+    @Id
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @GeneratedValue(generator = "uuid2")
+    @Type(type = "uuid-char")
+    @Column(name = "id", updatable = false, unique = true)
+    private UUID id;
 
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "created_at", nullable = false, updatable = false)
@@ -26,6 +36,10 @@ public abstract class BaseModel implements Serializable {
     @Column(name = "updated_at", nullable = false)
     @LastModifiedDate
     private Date updatedAt;
+
+    @Column(nullable = false, name = "deleted")
+    @Type(type = "boolean")
+    private Boolean deleted = Boolean.FALSE;
 
     public Date getCreatedAt() {
         return createdAt;
@@ -41,5 +55,31 @@ public abstract class BaseModel implements Serializable {
 
     public void setUpdatedAt(Date updatedAt) {
         this.updatedAt = updatedAt;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public BaseModel(Date createdAt, Date updatedAt, Boolean deleted) {
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.deleted = deleted;
+    }
+
+    BaseModel() {
+        //default constructor
     }
 }
